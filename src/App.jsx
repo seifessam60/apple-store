d49ce2730ee34b5a2a998fcae9d74a1f1ad23f68
seif essam <seifessam60@gmail.com>
2024-12-06 19:45:27 +0200

    add 3d models

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main className='bg-black'>
-      <Navbar /> <Hero /> <Highlights />
+      <Navbar /> <Hero /> <Highlights /> <Model />
     </main>
   );
 }
